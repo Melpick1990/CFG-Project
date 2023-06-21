606a526dc3f6586c3f0af5a6de00c9c0b7cb0d12
Melpick1990 <melpick1990@hotmail.com>
2023-06-21 20:35:03 +0100

    Final Updates
    All combined updates

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-//this may stick the dropdown label
-//$(function(){
 
-//    $(".dropdown-menu li a").click(function(){
 
-//      $(".btn:first-child").text($(this).text());
-//      $(".btn:first-child").val($(this).text());
+//this shows the correct map depending on the dropdown
 
-//   });
 
-//});
+function showMap() {
+   var city = document.getElementById('City').value;
+   if (city === 'valueSelect') {
+     document.getElementById('LondonMap').style.display = 'none';
+     document.getElementById('LiverpoolMap').style.display = 'none';
+     document.getElementById('HullMap').style.display = 'none';
+     document.getElementById('ManchesterMap').style.display = 'none';
+   } else if (city === 'valueLondon') {
+     document.getElementById('LondonMap').style.display = 'block';
+     document.getElementById('LiverpoolMap').style.display = 'none';
+     document.getElementById('HullMap').style.display = 'none';
+     document.getElementById('ManchesterMap').style.display = 'none';
+   } else if (city === 'valueManchester') {
+     document.getElementById('ManchesterMap').style.display = 'block';
+     document.getElementById('LiverpoolMap').style.display = 'none';
+     document.getElementById('HullMap').style.display = 'none';
+     document.getElementById('LondonMap').style.display = 'none';
+   } else if (city === 'valueLiverpool') {
+     document.getElementById('LiverpoolMap').style.display = 'block';
+     document.getElementById('ManchesterMap').style.display = 'none';
+     document.getElementById('HullMap').style.display = 'none';
+     document.getElementById('LondonMap').style.display = 'none';
+   } else {
+     document.getElementById('HullMap').style.display = 'block';
+     document.getElementById('LiverpoolMap').style.display = 'none';
+     document.getElementById('ManchesterMap').style.display = 'none';
+     document.getElementById('LondonMap').style.display = 'none';
+   }
+ }
 
-//this may show the correct map depending on the dropdown
-//$("#locationDropdown").on('show.bs.dropdown', function () {// do something…})
 
-function showMap(){
-    city = document.getElementById('City');
-    if (city == 'valueLondon')
-    {
-       document.getElementById('LondonMap').style.display='block'
-       document.getElementById('LiverpoolMap', 'HullMap', 'ManchesterMap').style.display='none'
-    }
-    else if (city == 'valueManchester')
-    {
-       document.getElementById('ManchesterMap').style.display='block'
-       document.getElementById('LiverpoolMap', 'HullMap', 'LondonMap').style.display='none'
-    }
-    else if (city == "valueLiverpool")
-    {
-       document.getElementById("LiverpoolMap").style.display='block'
-       document.getElementById("ManchesterMap", "HullMap", "LondonMap").style.display='none'
-    }
-    else
-    {
-       document.getElementById("HullMap").style.display='block'
-       document.getElementById("LiverpoolMap", "ManchesterMap", "LondonMap").style.display='none'
-    }
-    }
-    
-    
-    //stops the first carousel from changing slides when being hovered on
-    $("#howToHireCarousel").carousel({
-       interval: 300, pause: "hover"
-    });
- 
+
+//stops the first carousel from changing slides when being hovered on
+$("#howToHireCarousel").carousel({
+interval: 3000, pause: "hover"
+});
